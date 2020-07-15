ef33f6944d8af4d307f7c67f06da96b433ace7ed
marco sena <marcoacsena@gmail.com>
2020-07-15 18:47:57 -0300

    Código atualizado do DatePickerFragment.java.

--- app/src/main/java/com/example/tiapickerfordate/DatePickerFragment.java
+++ app/src/main/java/com/example/tiapickerfordate/DatePickerFragment.java
@@ -44,14 +44,14 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
      * @return A new instance of fragment DatePickerFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static DatePickerFragment newInstance(String param1, String param2) {
-        DatePickerFragment fragment = new DatePickerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+//    public static DatePickerFragment newInstance(String param1, String param2) {
+//        DatePickerFragment fragment = new DatePickerFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
 
 
     @Override
